5c805b62b7e51968dd25e6303dfbd6beac53126e
krishna-2305 <krishnagautam2305@gmail.com>
2026-08-11 10:48:26 +0530

    stack implementation program

---
+++ stack.c
@@ -0,0 +1,72 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct stack {
+    struct node *Top;
+    int capacity;
+    int size;
+};
+struct node{
+    int data;
+    struct node*next;
+};
+struct stack* createStack(int capacity){
+    struct stack* s=(struct stack*)malloc(sizeof(struct stack));
+    s->capacity=capacity;
+    s->size=0;
+    s->Top=NULL;
+    return s;
+};
+struct node* createNode(int data){
+    struct node* newNode=(struct node*)malloc(sizeof(struct node));
+    newNode->data=data;
+    newNode->next=NULL;
+    return newNode;
+};
+struct stack* push(struct stack* s,int data){
+    if(s->size==s->capacity){
+        printf("Stack Overflow\n");
+        return s;
+    }
+    struct node* newNode=createNode(data);
+    newNode->next=s->Top;
+    s->Top=newNode;
+    s->size++;
+    return s;
+};
+struct stack* pop(struct stack* s){
+    if(s->size==0){
+        printf("Stack Underflow\n");
+        return s;
+    }
+    struct node* temp=s->Top;
+    s->Top=s->Top->next;
+    free(temp);
+    s->size--;
+    return s;
+};
+int peek(struct stack* s){
+    if(s->size==0){
+        printf("Stack is empty\n");
+        return -1;                  
+    }
+    return s->Top->data;
+};
+struct stack* display(struct stack* s){
+    struct node* temp=s->Top;
+    while(temp!=NULL){
+        printf("%d\n",temp->data);
+        temp=temp->next;
+    }
+    return s;
+}; 
+int main(){
+    struct stack* s=createStack(5);
+    s=push(s,1);
+    s=push(s,2);
+    s=push(s,3);
+    s=display(s);
+    printf("Top element is %d\n",peek(s));
+    s=pop(s);
+    s=display(s);
+    return 0;
+} 
