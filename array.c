d288347145eeda49be2034a40e316f69c91ddecc
krishna-2305 <krishnagautam2305@gmail.com>
2026-09-10 17:39:03 +0530

    Array merge code added

--- array.c
+++ array.c
@@ -1,33 +1,3 @@
-//#include <stdio.h>
-
-//int main() {
-  //  int arr[100] = {10, 20, 30, 40, 50};
-   // int n = 5;
-    //int pos, value;
-
-    //printf("Enter position: ");
-    //scanf("%d", &pos);
-
-    //printf("Enter value: ");
-    //scanf("%d", &value);
-
-    // Shift elements to the right
-    //for (int i = n; i >= pos; i--) {
-    //    arr[i] = arr[i - 1];
-    //}
-
-    //arr[pos - 1] = value;
-    //n++;
-
-    //printf("Array after insertion:\n");
-
-    //for (int i = 0; i < n; i++) {
-    //    printf("%d ", arr[i]);
-    //}
-
-    //return 0;
-//}
-
 #include <stdio.h>
 
 void insertAtEnd(int arr[], int *n, int value)
@@ -51,6 +21,40 @@ int main()
     int n = 5;
 
     insertAtEnd(arr, &n, 60);
+
+    display(arr, n);
+
+    return 0;
+}
+
+#include <stdio.h>
+
+void insertAtPosition(int arr[], int *n, int pos, int value)
+{
+    for (int i = *n; i >= pos; i--)
+    {
+        arr[i] = arr[i - 1];
+    }
+
+    arr[pos - 1] = value;
+    (*n)++;
+}
+
+void display(int arr[], int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+
+int main()
+{
+    int arr[100] = {10, 20, 30, 40, 50};
+    int n = 5;
+
+    insertAtPosition(arr, &n, 3, 25);
 
     display(arr, n);
 
